f2051307cb71cf65c2876891b284be1e2fcc3257
Michael Spencer <ms53150@gmail.com>
2022-11-27 17:54:51 -0500

    Add example for arrow functions

--- Section_06-Functions/functions_basics.js
+++ Section_06-Functions/functions_basics.js
@@ -3,4 +3,17 @@ function my_function() {
 	console.log("Bye");
 }
 
-my_function();
+// my_function();
+
+// Arrow Functions
+const printMyName = (name) => {
+	console.log(name);
+};
+
+printMyName("Ren");
+
+const printMyDetails = (name, age) => {
+	console.log(name, age);
+};
+
+printMyDetails("Ren", 20);
