0bf9e3d2aa6d885d5198299c30aafee290568de0
Michael Spencer <ms53150@gmail.com>
2022-11-26 19:05:48 -0500

    Add examples for let and const variables

--- Section_01-Variables/main.js
+++ Section_01-Variables/main.js
@@ -1,3 +1,12 @@
 // Assign input/prompt to variable 'name'
 let name = prompt("What is your name?");
 console.log(name);
+
+// Decalaring variables
+var x = 5;
+let y = 6;
+const first_name = "Ren";
+
+// Rules
+// general rule: always declare variables with const
+// If you think the value of the variable can change, use let
