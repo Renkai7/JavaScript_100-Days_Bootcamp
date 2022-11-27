7661d459a51c93efcb44ab65a1207b3b8ccb9e45
Michael Spencer <ms53150@gmail.com>
2022-11-27 17:19:00 -0500

    Add methods for greet and getting age

--- Section_16-OOP/ES6 Classes/es6_classes.js
+++ Section_16-OOP/ES6 Classes/es6_classes.js
@@ -4,10 +4,25 @@
 // class declaration
 class PersonCl {
 	constructor(firstName, birthYear) {
+		// properties
 		this.firstName = firstName;
 		this.birthYear = birthYear;
+	}
+
+	// Methods
+	calcAge() {
+		console.log(2037 - this.birthYear);
+	}
+
+	greet() {
+		console.log(`Hey ${this.firstName}`);
 	}
 }
 
 const jessica = new PersonCl("Jessica", 1996);
 console.log(jessica);
+jessica.calcAge();
+jessica.greet();
+
+// Notes
+// Classes are executed in strict mode
